b3c40aaea6eef17feef460fcce4458acf0e4584e
Austin Louden <austinlouden@gmail.com>
2020-08-21 12:33:57 -0700

    Initial Commit

---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import NotifyTests
+
+var tests = [XCTestCaseEntry]()
+tests += NotifyTests.allTests()
+XCTMain(tests)
---
+++ Tests/NotifyTests/NotifyTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import Notify
+
+final class NotifyTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Notify().text, "Hello, World!")
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
